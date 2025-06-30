3c46d16f413750fd915a027551df59cb14e90c05
atoz-science <alisam9697@gmail.com>
2025-06-30 14:14:30 +0500

    app.js changig

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React and let it go and do some new.
+          i am updating App.js 
         </a>
       </header>
     </div>
